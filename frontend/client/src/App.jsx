eaa168a663ac09ea9956b946955476ae409a5480
Aryan Raj <adityaraj@Adityas-MacBook-Air.local>
2025-03-20 11:08:39 +0530

    added a joke component with dummy data

--- frontend/client/src/App.jsx
+++ frontend/client/src/App.jsx
@@ -1,7 +1,17 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import JokeCard from "./components/JokeCard";
 
 function App() {
-  return <Home />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/joke" element={<JokeCard />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
